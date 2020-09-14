1d1354640945c83528cd14b0a1e687ad1620a9fb
Abednego Kristanto <abedkristanto@gmail.com>
2020-09-14 19:24:46 +0700

    Adding interactive folium map

--- wiki_scrap.py
+++ wiki_scrap.py
@@ -14,25 +14,59 @@ def wiki_get_coordinates(places_list):
     for n, value in enumerate(places_list):
         try:
             coord = wikipedia.page(value).coordinates
-            #print(value, "coordinates is ", coord)
-        except (KeyError, wikipedia.exceptions.PageError):
+            keywords = value
+            #print(value + " NPP", "coordinates is ", coord)
+        except (KeyError, wikipedia.exceptions.PageError,
+        wikipedia.exceptions.DisambiguationError):
             #print("No wikipedia page named", value)
-            new_value = value[:-2] + " NPP"
+            new_value = value + " Nuclear Power Plant"
             #print("Search using keywords:", new_value)
             try:
                 coord = wikipedia.page(new_value).coordinates
+                keywords = new_value
                 #print(new_value, "coordinates is ", coord)
-            except (KeyError, wikipedia.exceptions.PageError):
+            except (KeyError, wikipedia.exceptions.PageError,
+            wikipedia.exceptions.DisambiguationError):
                 #print("No wikipedia page named", new_value)
-                new_value2 = new_value[:-6] + " NPP"
+                new_value2 = value[:-2] + " NPP"
                 #print("Search using keywords:", new_value2)
                 try:
                     coord = wikipedia.page(new_value2).coordinates
+                    keywords = new_value2
                     #print(new_value2, "coordinates is ", coord)
-                except (KeyError, wikipedia.exceptions.PageError):
+                except (KeyError, wikipedia.exceptions.PageError,
+                wikipedia.exceptions.DisambiguationError):
                     #print("No wikipedia page named", new_value2)
-                    not_found.append(value)
-                    coord = (0,0)
-        coordinate = [float(coord[0]), float(coord[1])]
+                    new_value3 = new_value2[:-4] + " Nuclear Power Plant"
+                    #print("Search using keywords:", new_value3)
+                    try:
+                        coord = wikipedia.page(new_value3).coordinates
+                        keywords = new_value3
+                        #print(new_value3, "coordinates is ", coord)
+                    except (KeyError, wikipedia.exceptions.PageError,
+                    wikipedia.exceptions.DisambiguationError):
+                        #print("No wikipedia page named", new_value3)
+                        new_value4 = new_value2[:-6] + " NPP"
+                        #print("Search using keywords:", new_value4)
+                        try:
+                            coord = wikipedia.page(new_value3).coordinates
+                            keywords = new_value3
+                            #print(new_value3, "coordinates is ", coord)
+                        except (KeyError, wikipedia.exceptions.PageError,
+                        wikipedia.exceptions.DisambiguationError):
+                            #print("No wikipedia page named", new_value3)
+                            new_value5 = new_value2[:-6] + " Nuclear Power Plant"
+                            #print("Search using keywords:", new_value5)
+                            try:
+                                coord = wikipedia.page(new_value5).coordinates
+                                keywords = new_value5
+                                #print(new_value5, "coordinates is ", coord)
+                            except (KeyError, wikipedia.exceptions.PageError,
+                            wikipedia.exceptions.DisambiguationError):
+                                #print("No wikipedia page named", new_value5)
+                                not_found.append(value)
+                                keywords = value
+                                coord = (0,0)
+        coordinate = [keywords, float(coord[0]), float(coord[1])]
         coord_list.append(coordinate)
     return coord_list, not_found
